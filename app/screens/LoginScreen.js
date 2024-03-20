64870b977ec0cf77fbdd8220cb2c0694fa183b79
nevaeh <xiny7803@gmail.com>
2024-03-20 14:08:03 +1300

    add Formik and Yup function.

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,47 +1,64 @@
-import React, { useState } from "react";
+import React from "react";
 import { Image, SafeAreaView, StyleSheet, View } from "react-native";
-
 import AppTextInput from "../components/AppTextInput";
 import AppButton from "../components/AppButton";
+import { Formik } from "formik";
+import * as Yup from "yup"
+import AppText from "../components/AppText";
+import color from "../config/color";
+
+const validationSchema = Yup.object().shape({
+    email: Yup.string().required().email().label("Email"),
+    password: Yup.string().required().min(8).label("Password")
+})
 
 function LoginScreen(props) {
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-
-
   return (
     <SafeAreaView>
       <View style={styles.container}>
-      <Image style={styles.logo} source={require("../assets/logo2.png")} />
-      <AppTextInput
-        autoCapitalize="none"
-        autoCorrect={false}
-        icon="email"
-        keyboardType="email-address"
-        onChangeText={(text) => setEmail(text)}
-        placeholder="Email"
-        textContentType="emailAddress"
-      />
-      <AppTextInput 
-      autoCapitalize="none"
-      autoCorrect={false}
-      icon='lock'
-      onChangeText={(text) => setPassword(text)}
-      placeholder="Password"
-      secureTextEntry
-      textContentType='password' // Ios will auto fill with its key chain
-      />
-      <AppButton 
-      title="Login"
-      onPress={()=>console.log(email, password)}/>
+        <Image style={styles.logo} source={require("../assets/logo2.png")} />
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={(values) => console.log(values)}
+          validationSchema={validationSchema}
+        >
+          {({ handleChange, handleSubmit, errors }) => (
+            <>
+              <AppTextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                icon="email"
+                keyboardType="email-address"
+                onChangeText={handleChange("email")}
+                placeholder="Email"
+                textContentType="emailAddress"
+              />
+              <AppText style={{color: 'red'}}>{errors.email}</AppText>
+              <AppTextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                icon="lock"
+                onChangeText={handleChange("password")}
+                placeholder="Password"
+                secureTextEntry
+                textContentType="password" // Ios will auto fill with its key chain
+              />
+              <AppText style={{color: 'red'}}>{errors.password}</AppText>
+              <AppButton
+                title="Login"
+                onPress={handleSubmit}
+              />
+            </>
+          )}
+        </Formik>
       </View>
     </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
-    container: {
-        padding: 20
-    },
+  container: {
+    padding: 20,
+  },
   logo: {
     width: 220,
     height: 100,
