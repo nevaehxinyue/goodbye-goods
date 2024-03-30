390c2666a6451b008dd68a50ef0605527184fe6c
nevaeh <xiny7803@gmail.com>
2024-03-30 17:25:18 +1300

    Add top stack navigator.

--- App.js
+++ App.js
@@ -31,25 +31,75 @@ import AppTextInput from "./app/components/TextInput";
 import AppPicker from "./app/components/Picker";
 import LoginScreen from "./app/screens/LoginScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
-import * as ImagePicker from 'expo-image-picker'
+import * as ImagePicker from "expo-image-picker";
 import ImageInput from "./app/components/ImageInput";
 import ImageInputList from "./app/components/ImageInputList";
-import ListingEditScreen from "./app/screens/ListingEditScreen"
-
+import ListingEditScreen from "./app/screens/ListingEditScreen";
+import Screen from "./app/components/Screen";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
 export default function App() {
- 
+  const Link = () => {
+    const navigation = useNavigation();
+    return (
+      <Button
+        title="Click"
+        onPress={() => navigation.navigate("TweetsDetails")}
+      />
+    );
+  };
+
+  const Tweets = ({ navigation }) => (
+    <Screen>
+      <Text>Tweets</Text>
+      {/* <Link /> */}
+      <Button
+        title="View Tweet"
+        onPress={() => navigation.navigate("TweetsDetails", { id: 1 })}
+      />
+    </Screen>
+  );
+
+  const TweetsDetails = ({ route }) => (
+    <Screen>
+      <Text>Tweets Details {route.params.id}</Text>
+    </Screen>
+  );
+
+  const Stack = createStackNavigator();
+  const StackNavigator = () => (
+    <Stack.Navigator screenOptions={{
+      headerStyle: { backgroundColor: "blue"}, 
+      headerTintColor: "white" ,
+    }}>
+      <Stack.Screen name="Tweets" component={Tweets} />
+      <Stack.Screen
+        name="TweetsDetails"
+        component={TweetsDetails}
+        options={({ route }) => ({
+          title: route.params.id,
+          headerStyle: { backgroundColor: "tomato"}, 
+          headerTintColor: "white" ,
+        })}
+      />
+    </Stack.Navigator>
+  );
+
   return (
-    <GestureHandlerRootView style={{flex: 1}} >
-      <ListingEditScreen />
-      
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+      {/* <ListingEditScreen /> */}
+
       {/* <WelcomeScreen /> */}
       {/* <MessagesScreen />  */}
       {/* <AccountScreen /> */}
       {/* <ListingsScreen /> */}
       {/* <LoginScreen /> */}
       {/* <RegisterScreen /> */}
-    
+
       {/* <ViewImageScreen /> */}
     </GestureHandlerRootView>
 
@@ -65,6 +115,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: color.white,
     alignItems: "center",
-    justifyContent: "center",  
+    justifyContent: "center",
   },
 });
